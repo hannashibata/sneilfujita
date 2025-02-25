af049893d192217295247bf7a7408390ec38385e
hannashibata <hanna.y.shibata@gmail.com>
2025-02-25 13:25:53 -1000

    updated map; added button;

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 // console.log("I am linked");
 
-// TOGGLE MENU
-const toggleMenu = document.querySelector('.toggle-menu');
-const toggleMenuButton = document.querySelector('.site-menu-button');
+// // TOGGLE MENU
+// const toggleMenu = document.querySelector('.toggle-menu');
+// const toggleMenuButton = document.querySelector('.site-menu-button');
 
-toggleMenuButton.onclick = function() {
+// toggleMenuButton.onclick = function() {
   
-  if (toggleMenu.getAttribute('data-menustate') === 'closed') {
+//   if (toggleMenu.getAttribute('data-menustate') === 'closed') {
     
-    // if closesd, open
-    toggleMenu.setAttribute('data-menustate','open');
+//     // if closesd, open
+//     toggleMenu.setAttribute('data-menustate','open');
   
-  } else {
+//   } else {
     
-    // else, close it
-    toggleMenu.setAttribute('data-menustate','closed');
+//     // else, close it
+//     toggleMenu.setAttribute('data-menustate','closed');
     
-  } 
+//   } 
   
-};
+// };
 
 // CLOSE THE TOGGLE MENU WHENEVER A USESR CLICKS ON A MENU LINK
 const toggleMenuLinks = document.querySelectorAll('.toggle-menu a');
